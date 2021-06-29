457aa7ed6508e68adeb039b4a82455e5f89c76a2
itismarcii <58007324+itismarcii@users.noreply.github.com>
2021-06-29 20:08:58 +0200

    Navbar creation

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -1,12 +1,81 @@
 import * as ko from "knockout";
-import { Observable } from "knockout";
+import {Observable} from "knockout";
 
-interface AppParams {}
+interface AppParams {
+}
 
+// Exchange for enum from main_panel if rdy
+enum Navigation {
+    home,
+    popular,
+    newMovies,
+    watchAgain
+}
 
 export default class Navbar {
 
-    constructor() {}
+    constructor() {
+    }
 
-    dispose() {}
+    dispose() {
+    }
+
+    onClickEventNavigation(navigation: number) {
+        switch (navigation) {
+            case Navigation.home:
+                this.disableEveryThingExcept(Navigation.home)
+                break;
+            case Navigation.popular:
+                this.disableEveryThingExcept(Navigation.popular)
+                break;
+            case Navigation.newMovies:
+                this.disableEveryThingExcept(Navigation.newMovies)
+                break;
+            case Navigation.watchAgain:
+                this.disableEveryThingExcept(Navigation.watchAgain)
+                break;
+        }
+    }
+
+    disableEveryThingExcept(active: number) {
+        let home = document.getElementById('navbar_home')
+        let popular = document.getElementById('navbar_popular')
+        let watchAgain = document.getElementById('navbar_watchAgain')
+        let newMovies = document.getElementById('navbar_newMovies')
+
+        switch (active) {
+            case Navigation.home:
+                if (home && popular && watchAgain && newMovies) {
+                    home.className = 'active'
+                    popular.className = 'default'
+                    newMovies.className = 'default'
+                    watchAgain.className = 'default'
+                }
+                break;
+            case Navigation.popular:
+                if (home && popular && watchAgain && newMovies) {
+                    home.className = 'default'
+                    popular.className = 'active'
+                    newMovies.className = 'default'
+                    watchAgain.className = 'default'
+                }
+                break;
+            case Navigation.newMovies:
+                if (home && popular && watchAgain && newMovies) {
+                    home.className = 'default'
+                    popular.className = 'default'
+                    newMovies.className = 'active'
+                    watchAgain.className = 'default'
+                }
+                break;
+            case Navigation.watchAgain:
+                if (home && popular && watchAgain && newMovies) {
+                    home.className = 'default'
+                    popular.className = 'default'
+                    newMovies.className = 'default'
+                    watchAgain.className = 'active'
+                }
+                break;
+        }
+    }
 }
